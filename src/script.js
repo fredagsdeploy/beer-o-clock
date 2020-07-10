d2eb09e37d5a8b5b959d46ec73c639735d7781c3
Johan Horvius <johan.horvius@astrazeneca.net>
2020-07-10 15:02:49 +0200

    Initial version

---
+++ src/script.js
@@ -0,0 +1,40 @@
+
+function nextDate() {
+  const today = new Date();
+  if(today.getDay() !== 5) {
+    today.setDate(today.getDate() + (5 - 1 - today.getDay() + 7) % 7 + 1);
+  }
+  today.setHours(15, 00, 00);
+  return today;
+}
+const target = nextDate().getTime();
+console.log(target);
+
+// countdown
+let timer = setInterval(function() {
+
+  // get today's date
+  const today = new Date().getTime();
+
+  // get the difference
+  const diff = target - today;
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // display
+  document.getElementById("timer").innerHTML =
+    "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
+}, 1000);
